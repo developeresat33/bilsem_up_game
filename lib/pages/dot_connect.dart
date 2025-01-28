61fab169d8ad91aa62067550d6afc2522cc29f04
developeresat33 <esat@esnafseninle.com>
2025-01-28 12:48:37 +0300

    28.01.2025

--- lib/pages/dot_connect.dart
+++ lib/pages/dot_connect.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:animated_background/animated_background.dart';
 import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter/material.dart';
 
@@ -8,7 +11,7 @@ class GameScreen extends StatefulWidget {
   State<GameScreen> createState() => _GameScreenState();
 }
 
-class _GameScreenState extends State<GameScreen> {
+class _GameScreenState extends State<GameScreen> with TickerProviderStateMixin {
   int gridSize = 5; // 5x5 grid
   late List<List<Color?>> gridColors; // 2D Grid Colors
   late List<dynamic> points; // Points from JSON
@@ -17,14 +20,17 @@ class _GameScreenState extends State<GameScreen> {
   Offset? startPoint; // Starting point of the current line
   List<Offset> currentPath = []; // Stores the current drawing path
   Map<Color, List<Offset>> completedPaths = {}; // Stores completed paths
-  // Example JSON Level Data
+
+  late AnimationController _glowAnimationController;
+  late AnimationController _animationController;
+  List<Offset>? animatedPoints; // The two points to animate
 
   @override
   void initState() {
     super.initState();
     levelData = {
       "level": 1,
-      "gridSize": 6,
+      "gridSize": 5,
       "points": [
         {"x": 0, "y": 0, "color": "red"},
         {"x": 2, "y": 0, "color": "red"},
@@ -34,10 +40,24 @@ class _GameScreenState extends State<GameScreen> {
         {"x": 4, "y": 4, "color": "green"}
       ]
     };
+
+    // Initialize Animation Controller
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 200),
+    )..addListener(() {
+        setState(() {});
+      });
+
+    _glowAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1500),
+    )..repeat(reverse: true);
+
     _initializeGameFromJson();
   }
 
-  void _initializeGameFromJson() {
+  void _initializeGameFromJson() async {
     completedPaths = {};
     gridSize = levelData["gridSize"];
     points = levelData["points"];
@@ -48,12 +68,21 @@ class _GameScreenState extends State<GameScreen> {
     gridColors =
         List.generate(gridSize, (_) => List.generate(gridSize, (_) => null));
 
+    animatedPoints = [];
     for (var point in points) {
       int x = point["x"];
       int y = point["y"];
       String colorName = point["color"];
       gridColors[x][y] = _getColorFromName(colorName);
+
+      animatedPoints!.add(Offset(x.toDouble(), y.toDouble()));
     }
+
+    _animationController.forward(from: 0);
+
+    await Future.delayed(const Duration(milliseconds: 300));
+
+    await _animationController.reverse();
 
     setState(() {});
   }
@@ -78,13 +107,11 @@ class _GameScreenState extends State<GameScreen> {
   }
 
   bool _isGameCompleted() {
-    // Tamamlanan yollar tüm noktaları bağlamalı
     for (var point in points) {
       final color = _getColorFromName(point['color']);
       final x = point['x'];
       final y = point['y'];
 
-      // Eğer bu renk için yol yoksa veya bu nokta visited değilse oyun tamamlanmamış
       if (!completedPaths.containsKey(color) ||
           !completedPaths[color]!
               .contains(Offset(x.toDouble(), y.toDouble()))) {
@@ -99,109 +126,126 @@ class _GameScreenState extends State<GameScreen> {
         .clamp(0, gridSize - 1);
     int y = (localPosition.dx ~/ (MediaQuery.of(context).size.width / gridSize))
         .clamp(0, gridSize - 1);
-setState(() {
+
+    setState(() {
       if (gridColors[x][y] != null) {
-      final color = gridColors[x][y];
+        final color = gridColors[x][y];
 
-      // Eğer bu renk zaten tamamlanmışsa, mevcut yolu temizle
-      if (completedPaths.containsKey(color)) {
-        _resetPathForColor(color!); // Bu renk grubunu temizle
+        if (completedPaths.containsKey(color)) {
+          _resetPathForColor(color!);
+        }
+
+        startPoint = Offset(x.toDouble(), y.toDouble());
+        currentPath.add(startPoint!);
       }
+    });
+  }
 
-      startPoint = Offset(x.toDouble(), y.toDouble());
-      currentPath.add(startPoint!);
+  void _handleTouchUpdate(Offset localPosition) {
+    if (startPoint == null) return;
+
+    int x = (localPosition.dy ~/ (MediaQuery.of(context).size.width / gridSize))
+        .clamp(0, gridSize - 1);
+    int y = (localPosition.dx ~/ (MediaQuery.of(context).size.width / gridSize))
+        .clamp(0, gridSize - 1);
+
+    Offset newPoint = Offset(x.toDouble(), y.toDouble());
+
+    if (visited[x][y] ||
+        (gridColors[x][y] != null &&
+            gridColors[x][y] !=
+                gridColors[startPoint!.dx.toInt()][startPoint!.dy.toInt()])) {
+      return;
     }
-});
-    // Eğer bir nokta başlangıç noktasıysa, işlemi başlat
 
-  }
-
-void _handleTouchUpdate(Offset localPosition) {
-  if (startPoint == null) return;
-
-  int x = (localPosition.dy ~/ (MediaQuery.of(context).size.width / gridSize))
-      .clamp(0, gridSize - 1);
-  int y = (localPosition.dx ~/ (MediaQuery.of(context).size.width / gridSize))
-      .clamp(0, gridSize - 1);
-
-  Offset newPoint = Offset(x.toDouble(), y.toDouble());
-
-  // Eğer yeni nokta zaten ziyaret edilmişse veya başka bir renk noktasına temas ediyorsa
-  if (visited[x][y] ||
-      (gridColors[x][y] != null &&
-          gridColors[x][y] !=
-              gridColors[startPoint!.dx.toInt()][startPoint!.dy.toInt()])) {
-    return; // Geçerli bir hareket değil, ekleme yapma
-  }
-
-  // Eğer geri dönme durumu varsa
-  if (currentPath.length > 1 && newPoint == currentPath[currentPath.length - 2]) {
-    currentPath.removeLast(); // Son eklenen noktayı sil
-    setState(() {});
-    return;
-  }
-
-  // Sadece yukarı, aşağı, sağ, sol hareketlere izin ver
-  if ((newPoint.dx == currentPath.last.dx &&
-          (newPoint.dy - currentPath.last.dy).abs() == 1) ||
-      (newPoint.dy == currentPath.last.dy &&
-          (newPoint.dx - currentPath.last.dx).abs() == 1)) {
-    if (!currentPath.contains(newPoint)) {
-      currentPath.add(newPoint); // Yeni noktayı ekle
+    if (currentPath.length > 1 &&
+        newPoint == currentPath[currentPath.length - 2]) {
+      currentPath.removeLast();
       setState(() {});
+      return;
+    }
+
+    if ((newPoint.dx == currentPath.last.dx &&
+            (newPoint.dy - currentPath.last.dy).abs() == 1) ||
+        (newPoint.dy == currentPath.last.dy &&
+            (newPoint.dx - currentPath.last.dx).abs() == 1)) {
+      if (!currentPath.contains(newPoint)) {
+        currentPath.add(newPoint);
+        setState(() {});
+      }
     }
   }
-}
-
 
   void _handleTouchEnd() async {
     if (startPoint != null && currentPath.isNotEmpty) {
       Offset lastPoint = currentPath.last;
 
-      // Renk eşleşmesi kontrolü
       if (gridColors[lastPoint.dx.toInt()][lastPoint.dy.toInt()] ==
           gridColors[startPoint!.dx.toInt()][startPoint!.dy.toInt()]) {
         final color =
             gridColors[startPoint!.dx.toInt()][startPoint!.dy.toInt()]!;
         completedPaths[color] = List.from(currentPath);
 
-        // Yolu işaretle
         for (var point in currentPath) {
           visited[point.dx.toInt()][point.dy.toInt()] = true;
         }
+
+        animatedPoints = [startPoint!, lastPoint];
+        _animationController.forward(from: 0);
+        await Future.delayed(const Duration(milliseconds: 100));
+        await _animationController.reverse();
+
         await FlameAudio.play('sound/connect.mp3', volume: 0.8);
       }
     }
 
-    // Sıfırla
     startPoint = null;
     currentPath.clear();
     setState(() {});
   }
 
-  /// Belirli bir renk grubunun yolunu temizler
   void _resetPathForColor(Color color) {
     if (!completedPaths.containsKey(color)) return;
 
-    // Bu renk için tamamlanan yol
     final path = completedPaths[color]!;
     for (var point in path) {
-      visited[point.dx.toInt()][point.dy.toInt()] =
-          false; // İşaretlemeyi kaldır
+      visited[point.dx.toInt()][point.dy.toInt()] = false;
     }
 
-    completedPaths.remove(color); // Tamamlanan yolu sil
+    completedPaths.remove(color);
     setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    _glowAnimationController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     double gridSizePx = MediaQuery.of(context).size.width / gridSize;
-
+    final glowOpacity =
+        Tween<double>(begin: 0.5, end: 1.0).animate(_glowAnimationController);
     return SafeArea(
-      child: Scaffold(
-        backgroundColor: Color.fromARGB(255, 19, 19, 19),
-        body: Stack(
+        child: Scaffold(
+      backgroundColor: const Color.fromARGB(255, 19, 19, 19),
+      body: AnimatedBackground(
+        vsync: this,
+        behaviour: RandomParticleBehaviour(
+            options: ParticleOptions(
+                image: Image.asset(
+                  'assets/img/space.png',
+                ),
+                particleCount: 30,
+                spawnOpacity: 0.1,
+                spawnMaxSpeed: 50,
+                spawnMaxRadius: 20,
+                spawnMinSpeed: 15,
+                baseColor: Colors.white),
+            paint: Paint()),
+        child: Stack(
           children: [
             Column(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -220,24 +264,26 @@ void _handleTouchUpdate(Offset localPosition) {
                   child: CustomPaint(
                     size: Size(gridSizePx * gridSize, gridSizePx * gridSize),
                     painter: GridPainter(
-                      gridSize: gridSize,
-                      gridColors: gridColors,
-                      completedPaths: completedPaths,
-                      currentPath: currentPath,
-                      gridSizePx: gridSizePx,
-                    ),
+                        gridSize: gridSize,
+                        gridColors: gridColors,
+                        completedPaths: completedPaths,
+                        currentPath: currentPath,
+                        gridSizePx: gridSizePx,
+                        animatedPoints: animatedPoints,
+                        scale: _animationController.value,
+                        glowOpacity: glowOpacity),
                   ),
                 ),
               ],
             ),
-            if (_isGameCompleted()) // Oyun tamamlandıysa düğmeyi göster
+            if (_isGameCompleted())
               Align(
                 alignment: Alignment.bottomCenter,
                 child: ElevatedButton(
                   onPressed: () {
                     setState(() {
                       _initializeGameFromJson();
-                      completedPaths.clear(); // Tüm tamamlanan yolları sıfırla
+                      completedPaths.clear();
                     });
                   },
                   child: const Text("Yeni Oyun"),
@@ -246,7 +292,7 @@ void _handleTouchUpdate(Offset localPosition) {
           ],
         ),
       ),
-    );
+    ));
   }
 }
 
@@ -256,6 +302,9 @@ class GridPainter extends CustomPainter {
   final Map<Color, List<Offset>> completedPaths;
   final List<Offset> currentPath;
   final double gridSizePx;
+  final List<Offset>? animatedPoints;
+  final double scale;
+  final Animation<double> glowOpacity;
 
   GridPainter({
     required this.gridSize,
@@ -263,28 +312,19 @@ class GridPainter extends CustomPainter {
     required this.completedPaths,
     required this.currentPath,
     required this.gridSizePx,
+    required this.animatedPoints,
+    required this.scale,
+    required this.glowOpacity,
   });
 
   @override
   void paint(Canvas canvas, Size size) {
-    // Grid arka planını doldur
     final backgroundPaint = Paint()
-      ..color = const Color.fromARGB(255, 45, 45, 45) // Hafif gri
+      ..color = const Color.fromARGB(255, 45, 45, 45)
       ..style = PaintingStyle.fill;
     canvas.drawRect(
         Rect.fromLTWH(0, 0, size.width, size.height), backgroundPaint);
 
-    // Gölge efekti ekle
-    final shadowPaint = Paint()
-      ..color = const Color.fromARGB(100, 0, 0, 0) // Yarı saydam siyah
-      ..maskFilter =
-          const MaskFilter.blur(BlurStyle.normal, 10); // Blur gölgesi
-    canvas.drawRect(
-      Rect.fromLTWH(0, 0, size.width, size.height + 10), // Hafif alttan taşır
-      shadowPaint,
-    );
-
-    // Grid çizimi
     final gridPaint = Paint()
       ..style = PaintingStyle.stroke
       ..strokeWidth = 2
@@ -297,34 +337,39 @@ class GridPainter extends CustomPainter {
       canvas.drawLine(Offset(0, offset), Offset(size.width, offset), gridPaint);
     }
 
-    // Noktaları çiz
     for (int i = 0; i < gridSize; i++) {
       for (int j = 0; j < gridSize; j++) {
         if (gridColors[i][j] != null) {
           final dotPaint = Paint()
             ..color = gridColors[i][j]!
             ..style = PaintingStyle.fill;
+
+          double circleRadius = gridSizePx / 3;
+
+          if (animatedPoints != null &&
+              animatedPoints!.contains(Offset(i.toDouble(), j.toDouble()))) {
+            circleRadius *= (1 + 0.2 * scale); // Animate scale
+          }
+
           canvas.drawCircle(
             Offset(j * gridSizePx + gridSizePx / 2,
                 i * gridSizePx + gridSizePx / 2),
-            gridSizePx / 3,
+            circleRadius,
             dotPaint,
           );
         }
       }
     }
 
-    // Tamamlanan yolları çiz (yumuşak dönüşler)
     completedPaths.forEach((color, path) {
       final pathPaint = Paint()
-        ..color = color
         ..style = PaintingStyle.stroke
-        ..strokeWidth = 8;
+        ..strokeWidth = 10
+        ..shader = _createGlowShader(color, path, gridSizePx);
 
       _drawSmoothPath(canvas, path, pathPaint);
     });
 
-    // Aktif yolu çiz (yumuşak dönüşler)
     if (currentPath.isNotEmpty) {
       final activePathPaint = Paint()
         ..color = gridColors[currentPath.first.dx.toInt()]
@@ -336,7 +381,29 @@ class GridPainter extends CustomPainter {
     }
   }
 
-  /// Yumuşak yolları çizmek için quadratic Bezier kullan
+  Shader _createGlowShader(Color color, List<Offset> path, double gridSizePx) {
+    final startPoint = path.first;
+    final endPoint = path.last;
+
+    final glowOpacityValue = glowOpacity.value;
+
+    final gradientColors = [
+      color.withOpacity(glowOpacityValue * 0.5),
+      color.withOpacity(glowOpacityValue),
+      color.withOpacity(glowOpacityValue * 0.5),
+    ];
+
+    return LinearGradient(
+      colors: gradientColors,
+      begin: Alignment.topLeft,
+      end: Alignment.bottomRight,
+      stops: [0.0, 0.5, 1.0],
+    ).createShader(Rect.fromPoints(
+      Offset(startPoint.dy * gridSizePx, startPoint.dx * gridSizePx),
+      Offset(endPoint.dy * gridSizePx, endPoint.dx * gridSizePx),
+    ));
+  }
+
   void _drawSmoothPath(Canvas canvas, List<Offset> path, Paint paint) {
     if (path.length < 2) return;
 
@@ -350,7 +417,6 @@ class GridPainter extends CustomPainter {
       final current = path[i];
       final next = path[i + 1];
 
-      // Calculate the mid-point for smooth transitions
       final midX = (current.dy + next.dy) / 2;
       final midY = (current.dx + next.dx) / 2;
 
@@ -362,7 +428,6 @@ class GridPainter extends CustomPainter {
       );
     }
 
-    // Son noktayı çiz
     final lastPoint = path.last;
     pathToDraw.lineTo(
       lastPoint.dy * gridSizePx + gridSizePx / 2,
